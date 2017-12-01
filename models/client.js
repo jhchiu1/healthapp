dc00dd3d8f6c2b05c6bca66e069b7ab2a2da379d
juliechiu <jhchiu1@gmail.com>
2017-12-01 13:13:00 -0600

    Added dropdown list for sex field in client record form, age field with validation, date field

--- models/client.js
+++ models/client.js
@@ -4,22 +4,24 @@ var uniqueValidator = require('mongoose-unique-validator');
 /* Information about a client, and dates this client's record was updated */
 
 var clientSchema = new mongoose.Schema({
-    first: { type: String, required: [true, 'Client FIRST name is required.'],
+    first: {
+        type: String, required: [true, 'Client FIRST name is required.'],
         unique: true,
         uniqueCaseInsensitive: true,
         validate: {
-            validator: function(n) {
+            validator: function (n) {
                 return n.length >= 2;
             },
             message: '{VALUE} is not valid, client FIRST name must be at least 2 letters'
         }
     },             // Validation where unique client name required being at least 2 letters long.
 
-    last: { type: String, required: [true, 'Client LAST name is required.'],
+    last: {
+        type: String, required: [true, 'Client LAST name is required.'],
         unique: true,
         uniqueCaseInsensitive: true,
         validate: {
-            validator: function(n) {
+            validator: function (n) {
                 return n.length >= 2;
             },
             message: '{VALUE} is not valid, client LAST name must be at least 2 letters'
@@ -27,24 +29,34 @@ var clientSchema = new mongoose.Schema({
     },
     first: String,
     last: String,
-    Male: { type: Boolean, default: false},        // Client male or female? Defaults to unchecked to indicate female
-
+    sex: {type: String, enum: ['Male', 'Female']},        // Client male or female? Defaults to unchecked to indicate female
+    age: {
+        type: Number,
+        min: [5, 'Should be at least 5 years old'],
+        max: [125, 'Should not be more than 125 years old.']
+    },
     height: {
         type: Number,
-        min: [2, 'Should be at least 2 feet'],
-        max: [10, 'Should not be more than 10 feet.'] },    // At least 24 inches, no more than 150 inches.
+        min: [3, 'Should be at least 3 feet'],
+        max: [10, 'Should not be more than 10 feet.']
+    },    // At least 24 inches, no more than 150 inches.
     weight: {
         type: Number,
         min: [50, 'Should be at least 50 lbs.'],
-        max: [700, 'Should not be more than 700 lbs.'] },    // At least 50 lbs, no more than 700 lbs.
+        max: [700, 'Should not be more than 700 lbs.']
+    },    // At least 50 lbs, no more than 700 lbs.
     heart: {
         type: Number,
         min: [30, 'Should be at least 30 bpm.'],
-        max: [250, 'Should not be more than 250 bpm.'] },    // At least 30 bpm, no more than 250 bpm
+        max: [250, 'Should not be more than 250 bpm.']
+    },    // At least 30 bpm, no more than 250 bpm
     notes: String,
+    //datesUpdate: {type: Date, default: Date.now},
+
     datesUpdate: [ {
         type: Date,
         required: true,
+        default: Date.now,
         validate: {
             validator: function(d) {
                 if (!d) { return false; }
